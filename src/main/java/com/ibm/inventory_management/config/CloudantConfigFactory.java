61e3e3bfaaa2fa4c577e7acf1270241e5c384e88
Sean Sundberg <seansund@us.ibm.com>
2021-10-29 08:19:42 -0500

    Updates cloudant config
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/main/java/com/ibm/inventory_management/config/CloudantConfigFactory.java
+++ src/main/java/com/ibm/inventory_management/config/CloudantConfigFactory.java
@@ -1,13 +1,20 @@
 package com.ibm.inventory_management.config;
 
 import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+@Profile("cloudant")
 @Component
 public class CloudantConfigFactory {
+    private static final Logger log = LoggerFactory.getLogger(CloudantConfigFactory.class);
+
     @Bean
     public CloudantConfig buildCloudantConfig() throws IOException {
         return buildConfigFromBinding(
@@ -17,7 +24,11 @@ public class CloudantConfigFactory {
     }
 
     protected String loadCloudantConfig() throws IOException {
-        return System.getProperty("CLOUDANT_CONFIG") != null
+        if (System.getProperty("CLOUDANT_CONFIG") != null) {
+            log.warn("Config not found at CLOUDANT_CONFIG");
+        }
+
+       return System.getProperty("CLOUDANT_CONFIG") != null
                 ? System.getProperty("CLOUDANT_CONFIG")
                 : loadCloudantMappingFromLocalDev().getCloudantConfig();
     }
@@ -44,10 +55,17 @@ public class CloudantConfigFactory {
             return new CloudantConfig();
         }
 
+        log.debug("Building cloudant config from value: " + binding);
+
         final CloudantConfig baseConfig = mapper.readValue(binding, CloudantConfig.class);
 
         if (baseConfig == null) {
-            return new CloudantConfig();
+            log.warn("Unable to parse Cloudant config: " + binding);
+            return new CloudantConfig()
+                .withDatabaseName(databaseName)
+                .withUrl("https://0e66b895-2249-4980-b392-fb6e2a40370c-bluemix.cloudantnosqldb.appdomain.cloud")
+                .withUsername("0e66b895-2249-4980-b392-fb6e2a40370c-bluemix")
+                .withApikey("uebCSAZv8pO4kdzuf4nvtqgLWWsL6hSDdZZHmQl_-jB_");
         }
 
         return baseConfig.withDatabaseName(databaseName);
